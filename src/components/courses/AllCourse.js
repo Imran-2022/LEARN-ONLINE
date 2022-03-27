c19ea3f13fb414fe745b57f90f43eb31ef991cd3
Imran-2022 <mdimranulhaque.2022@gmail.com>
2022-03-28 01:29:24 +0600

    update courses

--- src/components/courses/AllCourse.js
+++ src/components/courses/AllCourse.js
@@ -6,18 +6,30 @@ const AllCourse = () => {
             <section class="course w-75 m-auto text-center">
                 <h1 className="fs-4 fw-bold">Courses we offer</h1>
                 <p className="text-dark fs-6 line-height-lg p-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis in provident, accusamus blanditiis magni inventore!</p>
-                <div class="mt-5 d-flex justify-content-between courses">
-                    <div class="course-details rounded mb-5 p-3">
-                        <h3 className="text-center fw-bold mt-3 mb-3 p-2">Lorem ipsum dolor</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quisquam eaque laboriosam a illum, soluta sequi aperiam delectus quo molestiae assumenda incidunt eveniet! Incidunt repellat dolorem earum, amet at dolorum.</p>
+                <div class="mt-5 d-flex flex-wrap justify-content-between courses">
+                    <div class="course-details rounded">
+                        <img width="100%" height="40%" src="https://analyticsinsight.b-cdn.net/wp-content/uploads/2020/11/Artificial-Intelligence-5.jpg" alt="" />
+                       <div className="p-3 details-in">
+                       <h3 className="text-center fw-bold mt-3 mb-3 ">Lorem ipsum dolor</h3>
+                        <p className='text-start'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quisquam eaque laboriosam a illum, soluta sequi aperiam delectus quo molestiae assumenda incidunt eveniet! Incidunt repellat dolorem earum, amet at dolorum.</p>
+                        <button className="btn btn-primary w-100 ">LEARN MORE</button>
+                       </div>
                     </div>
-                    <div class="course-details rounded mb-5 p-3">
-                        <h3 className="text-center fw-bold mt-3 mb-3 p-2">Lorem ipsum dolor</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quisquam eaque laboriosam a illum, soluta sequi aperiam delectus quo molestiae assumenda incidunt eveniet! Incidunt repellat dolorem earum, amet at dolorum.</p>
+                    <div class="course-details rounded">
+                        <img width="100%" height="40%" src="https://analyticsinsight.b-cdn.net/wp-content/uploads/2020/11/Artificial-Intelligence-5.jpg" alt="" />
+                       <div className="p-3 details-in">
+                       <h3 className="text-center fw-bold mt-3 mb-3 ">Lorem ipsum dolor</h3>
+                        <p className='text-start'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quisquam eaque laboriosam a illum, soluta sequi aperiam delectus quo molestiae assumenda incidunt eveniet! Incidunt repellat dolorem earum, amet at dolorum.</p>
+                        <button className="btn btn-primary w-100 ">LEARN MORE</button>
+                       </div>
                     </div>
-                    <div class="course-details rounded mb-5 p-3">
-                        <h3 className="text-center fw-bold mt-3 mb-3 p-2">Lorem ipsum dolor</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quisquam eaque laboriosam a illum, soluta sequi aperiam delectus quo molestiae assumenda incidunt eveniet! Incidunt repellat dolorem earum, amet at dolorum.</p>
+                    <div class="course-details rounded">
+                        <img width="100%" height="40%" src="https://analyticsinsight.b-cdn.net/wp-content/uploads/2020/11/Artificial-Intelligence-5.jpg" alt="" />
+                       <div className="p-3 details-in">
+                       <h3 className="text-center fw-bold mt-3 mb-3 ">Lorem ipsum dolor</h3>
+                        <p className='text-start'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea quisquam eaque laboriosam a illum, soluta sequi aperiam delectus quo molestiae assumenda incidunt eveniet! Incidunt repellat dolorem earum, amet at dolorum.</p>
+                        <button className="btn btn-primary w-100 ">LEARN MORE</button>
+                       </div>
                     </div>
                 </div>
             </section>
